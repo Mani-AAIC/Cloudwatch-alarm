6535cf23314d4b530a64809a33891ff2ede7cd32
Mani-AAIC <106646325+Mani-AAIC@users.noreply.github.com>
2022-06-02 12:42:01 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+resource "aws_cloudwatch_metric_alarm" "AAIC" {
+  alarm_name                = "terraform-test-AAIC"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "ASIA5CA4YRK632KA567V"
+  secret_key = "t+d+QHX8SgYIopF29plU6cKQjPevFjRzClSC1AaW"
+ }
